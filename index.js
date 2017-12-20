ab6f9ccdcbea3050b94a9a08913de77442bd3010
YuanYuan Li <yuanyuan.li@nshah.jet.local>
2017-12-20 09:25:48 -0500

    init server

---
+++ index.js
@@ -0,0 +1,12 @@
+// nodejs runtime only support commonJS, no support ES6 import
+const express = require('express'); 
+const app = express();
+const PORT = process.env.port || 5000;
+
+app.get('/', (req, res) => {
+    res.send('here');
+})
+
+app.listen(PORT, () => {
+    console.log(`Port listen at ${PORT}`);
+})

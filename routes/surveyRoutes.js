0ae01cbfba19b59382f5c40bb93a9c9b7ccd2de3
YuanYuan Li <yuanyuan.li@yli-m.fios-router.home>
2018-01-29 20:47:11 -0500

    add sendgrid webhook

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,4 +1,6 @@
-
+const _ = require('lodash');
+const Path = require('path-parser');
+const { URL } = require('url');
 const requireAuth = require('../middlewares/requireAuth');
 const requireCredits = require('../middlewares/requireCredits');
 const mongoose = require('mongoose');
@@ -18,6 +20,53 @@ module.exports = (app) => {
 
     app.get('/api/surveys/:surveyId/:choice', (req, res) => {
         res.send('Thanks for voting!');
+    });
+
+    app.post('/api/survey/webhooks', (req, res) => {
+        // const events = _.map(req.body, ({email, url})=>{
+        //     const pathname = new URL(url).pathname;
+        //     const p = new Path('/api/surveys/:surveyId/:choice');
+        //     const match = p.test(pathname);  // {surveyId: dd, choice: dd}
+        //     if(match){
+        //         return {
+        //             email: email,
+        //             surveyId: match.surveyId,
+        //             choice: match.choice };
+        //     }
+        // })
+
+        // const compactEvents = _.compact(events); // remove undefined
+        // const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+
+        const p = new Path('/api/surveys/:surveyId/:choice');        
+        _.chain(req.body)
+        .filter(value => !!value.url)
+        .map(({url, email})=>{
+            const match = p.test(new URL(url).pathname);  // {surveyId: dd, choice: dd}
+            if(match){
+                return {
+                    email: email,
+                    surveyId: match.surveyId,
+                    choice: match.choice };
+            }
+        })
+        .compact()
+        .uniqBy('email', 'surveyId')
+        .each( ({surveyId, email, choice}) =>{
+            Survey.updateOne({
+                _id: surveyId,
+                recipients: {
+                    $elemMatch: { email: email, responded: false}
+                }
+            }, {
+                $inc: { [choice]: 1},
+                $set: { 'recipients.$.responded': true},
+                lastResponded: new Date()
+            }).exec();
+        })
+        .value();
+        
+        res.send({});
     });
 
     app.post('/api/surveys', requireAuth, requireCredits, async (req, res) => {
